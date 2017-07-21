e5f8a0284c460f64ad47ad206269d57cf18fca16
krza <krza@mmmi.sdu.dk>
2017-07-21 14:40:05 -0700

    Add InternalGains stub

---
+++ epquery/edit/internal_gains.py
@@ -0,0 +1,17 @@
+from epquery import BasicEdit
+from epquery import utilities
+
+
+class InternalGains(BasicEdit):
+
+    def lights(self, mask):
+        # TODO: Move from std.py
+        pass
+
+    def people(self, mask):
+        # TODO: Move from std.py
+        pass
+    
+    def electric_equipment(self, mask):
+        # TODO: Move from std.py
+        pass
